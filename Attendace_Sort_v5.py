d44b1b51f1379daf72be30b24c9f34fdf49b6a41
kayser <kayser2202@gmail.com>
2024-05-19 10:27:04 +0600

    insert 1st three row name and date

--- Attendace_Sort_v5.py
+++ Attendace_Sort_v5.py
@@ -49,63 +49,76 @@ def process_data(assignee_name, report_start, report_end, file_path):
     if 'Sheet1' not in writer.sheets:
         writer.book.create_sheet('Sheet1')
 
+    # Insert a row at the beginning of the Excel sheet
+    # writer.sheets['Sheet1'].insert_rows(1)
+    cell = writer.sheets['Sheet1'].cell(row=1, column=1, value=f"Weekly Task Report")
+    writer.sheets['Sheet1'].merge_cells(start_row=1, start_column=1, end_row=1, end_column=4)
+    cell.alignment = cell.alignment.copy(wrapText=True, horizontal="center", vertical="center")
+    cell.font = cell.font.copy(bold=True, size=18)
+        
+    cell = writer.sheets['Sheet1'].cell(row=2, column=1, value=f"{assignee_name}")
+    writer.sheets['Sheet1'].merge_cells(start_row=2, start_column=1, end_row=2, end_column=4)
+    cell.alignment = cell.alignment.copy(wrapText=True, horizontal="center", vertical="center")
+    cell.font = cell.font.copy(bold=True, size=18)
+
+    cell = writer.sheets['Sheet1'].cell(row=3, column=1, value=f"Reporting Dates: {report_start} to {report_end}")
+    writer.sheets['Sheet1'].merge_cells(start_row=3, start_column=1, end_row=3, end_column=4)
+    cell.alignment = cell.alignment.copy(wrapText=True, horizontal="center", vertical="center")
+    cell.font = cell.font.copy(bold=True, size=14)
+    
+   
    
     for folder_name, group in grouped:
         if not group.empty:
-    
-            group_sorted = group.sort_values(by='Due Date Text')
+            group = group.rename(columns={'Start Date Text': 'Start Time', 'Due Date Text': 'End Time'})
+                        
+            group['Start Time'] = pd.to_datetime(group['Start Time'], format='%m/%d/%Y, %I:%M:%S %p GMT+6').dt.strftime('%d/%m/%Y, %H:%M')
+            group['End Time'] = pd.to_datetime(group['End Time'], format='%m/%d/%Y, %I:%M:%S %p GMT+6').dt.strftime('%d/%m/%Y, %H:%M')
 
-           
-            group_sorted = group_sorted.rename(columns={'Start Date Text': 'Start Time', 'Due Date Text': 'End Time'})
-
+            group_sorted = group.sort_values(by='End Time', ascending=True)
             
             start_row = writer.sheets['Sheet1'].max_row + 3 if writer.sheets['Sheet1'].max_row is not None else 0
-
-            
-            group_sorted['Start Time'] = pd.to_datetime(group_sorted['Start Time'], format='%m/%d/%Y, %I:%M:%S %p GMT+6').dt.strftime('%d/%m/%Y, %H:%M')
-            group_sorted['End Time'] = pd.to_datetime(group_sorted['End Time'], format='%m/%d/%Y, %I:%M:%S %p GMT+6').dt.strftime('%d/%m/%Y, %H:%M')
-
-           
+                      
             group_sorted.to_excel(writer, sheet_name='Sheet1', startrow=start_row, index=False)
 
-      
             writer.sheets['Sheet1'].insert_rows(start_row)
             cell = writer.sheets['Sheet1'].cell(row=start_row, column=1, value=f"{folder_name}")
             cell.font = copy(cell.font)
             cell.font = cell.font.copy(bold=True, size=14)
 
-          
             last_column = writer.sheets['Sheet1'].max_column
             writer.sheets['Sheet1'].merge_cells(start_row=start_row, start_column=1, end_row=start_row, end_column=last_column)
-
+            # cell.height = 17
             
             cell.alignment = copy(cell.alignment)
             cell.alignment = cell.alignment.copy(horizontal="left", vertical="center")
+    
 
     try:
         writer.close()
 
-      
         wb = load_workbook(output_file)
-        ws = wb['Sheet1']
+        ws = wb['Sheet1']        
 
         # Define column widths
-        column_widths = {'A': 55, 'B': 10.5, 'C': 18, 'D': 18, 'E': 23}
+        column_widths = {'A': 37, 'B': 16, 'C': 16, 'D': 20}
 
         # Set column widths
         for column, width in column_widths.items():
             ws.column_dimensions[column].width = width
 
+        ws.row_dimensions[1].height = 21
+        ws.row_dimensions[2].height = 21
+        ws.row_dimensions[3].height = 17
      
         for row in ws.iter_rows(min_row=1, min_col=1, max_row=ws.max_row, max_col=ws.max_column):
             for cell in row:
                 cell.alignment = copy(cell.alignment)
                 cell.alignment = cell.alignment.copy(wrapText=True, vertical="center")
 
-   
         from openpyxl.worksheet.page import PageMargins
         ws.page_margins = PageMargins(left=0.5, right=0.5, top=0.5, bottom=0.5, header=0.5, footer=0.5)
-        ws.page_setup.orientation = ws.ORIENTATION_LANDSCAPE
+        ws.page_setup.orientation = ws.ORIENTATION_PORTRAIT
         ws.page_setup.paperSize = ws.PAPERSIZE_A4
 
        
